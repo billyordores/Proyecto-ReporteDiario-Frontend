9a3d698d72b34f17d7f49172e080c8b37652e984
Rene Beca <juan.ixcopal@alumnos.uneatlantico.es>
2022-03-10 18:00:17 +0100

    Added header

--- dailyreport/src/App.js
+++ dailyreport/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './css/App.css'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hola mundo</h1>
-    </div>
+    <header className="Header">
+      <img className='Logo' src={process.env.PUBLIC_URL + 'Logo_EIA.png'} />
+      <h1 className="Title">Plataforma de Gestión para Mantenimiento Aulas</h1>
+    </header>
   );
 }
 
