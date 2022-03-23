946fcf6c606ab4cd2ba0b472f5a08d7b9acc8261
Rene Beca <juan.ixcopal@alumnos.uneatlantico.es>
2022-03-23 14:32:08 +0100

    button plantas

--- dailyreport/src/App.js
+++ dailyreport/src/App.js
@@ -13,7 +13,7 @@ function App() {
     
       <Routes>
 
-        <Route path='/home' element={<Home/>} />
+        <Route path='/' element={<Home/>} />
 
         <Route path='/plantas' element={<Plantas/>} />
 
